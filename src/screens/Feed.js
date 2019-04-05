09862a64b4c7c15e4eb822d08b8856ebc3120f0d
victormf123 <victormf.df@hotmail.com>
2019-04-04 23:03:53 -0300

    first commit

---
+++ src/screens/Feed.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { StyleSheet, FlatList, View } from 'react-native'
+import Header from '../components/Header'
+import Post from '../components/Post'
+
+class Feed extends Component {
+    state = {
+        posts: [{
+            id: Math.random(),
+            neckname: 'Rafael Paereira Filho',
+            email: 'rafaelprrflh@gmail.com',
+            image: require('../../assets/imgs/fence.jpg'),
+            comments: [{
+                nickname: 'John Ray Sheldon',
+                comment: 'Stunning!'
+            },{
+                nickname: 'Ana Julia Arruda',
+                comment: 'Foto linda! Onde foi tirada?'
+            }]
+        },{
+            id: Math.random(),
+            neckname: 'Francisco Leandro Lima',
+            email: 'fllima@gmail.com',
+            image: require('../../assets/imgs/bw.jpg'),
+            comments: []
+        }] 
+    }
+    render() {
+        return (
+            <View style={StyleSheet.container}>
+                <Header />
+                <FlatList 
+                    data={this.state.posts}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={({item}) => <Post key={item.id} {...item}/>} />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF' 
+    }
+})
+
+export default Feed
