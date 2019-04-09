215bd64d08f909cd994cf60c889dc27a95801d24
victormf123 <victormf.df@hotmail.com>
2019-04-08 22:44:25 -0300

    resolvendo Native module VectorIconsModule tried to override VectorIconsModule for the module name RNVectorIconsModule

--- android/app/src/main/java/com/lambe/MainApplication.java
+++ android/app/src/main/java/com/lambe/MainApplication.java
@@ -5,8 +5,6 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,8 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new ImagePickerPackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage()
       );
